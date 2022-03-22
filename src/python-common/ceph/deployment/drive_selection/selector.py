1df390511f0662aeb50fdffe468e5d51a8495747
Guillaume Abrioux <gabrioux@redhat.com>
2022-03-22 10:05:31 +0000

    wip
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

--- src/python-common/ceph/deployment/drive_selection/selector.py
+++ src/python-common/ceph/deployment/drive_selection/selector.py
@@ -121,6 +121,12 @@ class DriveSelection(object):
         for disk in self.disks:
             logger.debug("Processing disk {}".format(disk.path))
 
+            if not disk.available and ("Has GPT headers" in disk.rejected_reasons or "Has BlueStore device label" in disk.rejected_reasons):
+                logger.debug(
+                    f'Ignoring disk {disk.path}. Disk is unavailable due to {disk.rejected_reasons}'
+                )
+                continue
+
             if not self._has_mandatory_idents(disk):
                 logger.debug(
                     "Ignoring disk {}. Missing mandatory idents".format(
